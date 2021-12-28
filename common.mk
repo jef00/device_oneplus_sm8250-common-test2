ee4a0b9e9fecf66f2ced19424966dbe960f8ed41
jeffrey <jeffrey_jagram_90@hotmail.com>
2021-12-28 23:59:52 +0000

    Remove Touch

--- common.mk
+++ common.mk
@@ -429,10 +429,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.oneplus
-
 # tri-state-key
 PRODUCT_PACKAGES += \
     KeyHandler \
